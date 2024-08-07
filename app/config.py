d184f3acfe02d1a474c437a952ab0dc213acc56f
Felipe Alvarado <felipe@safe.global>
2024-08-07 14:08:09 +0200

    Update log format to follow the Safe guidelines

--- app/config.py
+++ app/config.py
@@ -2,6 +2,8 @@
 Base settings file for FastApi application.
 """
 
+import json
+import logging.config
 import os
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -19,6 +21,41 @@ class Settings(BaseSettings):
     DEFAULT_SIWE_MESSAGE_STATEMENT: str = (
         "Welcome to Safe! I accept the Terms of Use: https://safe.global/terms."
     )
+    LOG_LEVEL: str = "INFO"
 
 
 settings = Settings()
+
+
+LOG_FORMAT = {
+    "level": "%(levelname)s",
+    "timestamp": "%(asctime)s",
+    "message": "%(message)s",
+    "context": "",
+    "messageContext": "",
+}
+
+LOG_CONFIG = {
+    "version": 1,
+    "disable_existing_loggers": True,
+    "formatters": {"default": {"format": json.dumps(LOG_FORMAT)}},
+    "handlers": {
+        "console": {
+            "formatter": "default",
+            "class": "logging.StreamHandler",
+            "stream": "ext://sys.stdout",
+            "level": settings.LOG_LEVEL,
+        }
+    },
+    "root": {"handlers": ["console"], "level": settings.LOG_LEVEL},
+    "loggers": {
+        "gunicorn": {"propagate": True},
+        "gunicorn.access": {"propagate": True},
+        "gunicorn.error": {"propagate": True},
+        "uvicorn": {"propagate": True},
+        "uvicorn.access": {"propagate": True},
+        "uvicorn.error": {"propagate": True},
+    },
+}
+
+logging.config.dictConfig(LOG_CONFIG)
