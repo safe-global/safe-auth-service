3989d1641b1557c17fa929efb19165af145aecd3
Felipe Alvarado <felipe@safe.global>
2024-08-07 14:07:20 +0200

    Add gunicorn server to run app

--- docker/web/run_web.sh
+++ docker/web/run_web.sh
@@ -7,5 +7,6 @@ DOCKER_SHARED_DIR=/nginx
 rm -rf $DOCKER_SHARED_DIR/*
 cp -r static/ $DOCKER_SHARED_DIR/
 
-echo "==> $(date +%H:%M:%S) ==> Running Uvicorn... "
-exec uvicorn app.main:app --host 0.0.0.0 --port 8888 --proxy-headers --uds $DOCKER_SHARED_DIR/uvicorn.socket
+echo "==> $(date +%H:%M:%S) ==> Running Gunicorn with Uvicorn... "
+# https://fastapi.tiangolo.com/deployment/server-workers/#server-workers-gunicorn-with-uvicorn
+exec gunicorn app.main:app -k uvicorn.workers.UvicornWorker -b unix:$DOCKER_SHARED_DIR/uvicorn.socket -b 0.0.0.0:8888
