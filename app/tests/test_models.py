cfbd8a549a8c7b96800d82f79f2d610cfd9d9a80
Felipe Alvarado <felipe@safe.global>
2024-08-19 16:13:17 +0200

    Add SiweMessageRequestFactory in test models

--- app/tests/test_models.py
+++ app/tests/test_models.py
@@ -2,7 +2,8 @@ import unittest
 
 from pydantic import HttpUrl, ValidationError
 
-from ..models import Nonce, SiweMessageRequest, SiweMessageVerificationRequest
+from ..models import Nonce, SiweMessageVerificationRequest
+from .factories import SiweMessageRequestFactory
 
 
 class TestNonce(unittest.TestCase):
@@ -22,57 +23,29 @@ class TestNonce(unittest.TestCase):
 
 class TestSiweMessageRequest(unittest.TestCase):
     def test_valid_siwe_message_request(self):
-        siwe_message_request = SiweMessageRequest(
-            domain="example.com",
-            address="0x32Be343B94f860124dC4fEe278FDCBD38C102D88",
-            chain_id=1,
-            uri=HttpUrl("https://example.com/"),
-            statement="Test statement",
-        )
+        siwe_message_request = SiweMessageRequestFactory.create()
         self.assertEqual(siwe_message_request.domain, "example.com")
         self.assertEqual(
             siwe_message_request.address, "0x32Be343B94f860124dC4fEe278FDCBD38C102D88"
         )
         self.assertEqual(siwe_message_request.chain_id, 1)
-        self.assertEqual(str(siwe_message_request.uri), "https://example.com/")
+        self.assertEqual(str(siwe_message_request.uri), "https://valid-url.com/")
         self.assertEqual(siwe_message_request.statement, "Test statement")
 
     def test_invalid_domain(self):
         with self.assertRaises(ValidationError):
-            SiweMessageRequest(
-                domain="example.com/invalid",
-                address="0x32Be343B94f860124dC4fEe278FDCBD38C102D88",
-                chain_id=1,
-                uri=HttpUrl("https://example.com"),
-            )
+            SiweMessageRequestFactory.create(domain="example.com/invalid")
 
     def test_invalid_address(self):
         with self.assertRaises(ValidationError):
-            SiweMessageRequest(
-                domain="example.com",
-                address="0xInvalidEthereumAddress",
-                chain_id=1,
-                uri=HttpUrl("https://example.com"),
-                statement="Test statement",
-            )
+            SiweMessageRequestFactory.create(address="0xInvalidEthereumAddress")
 
     def test_invalid_uri(self):
         with self.assertRaises(ValidationError):
-            SiweMessageRequest(
-                domain="example.com",
-                address="0x32Be343B94f860124dC4fEe278FDCBD38C102D88",
-                chain_id=1,
-                uri=HttpUrl("invalid-url"),
-                statement="Test statement",
-            )
+            SiweMessageRequestFactory.create(uri=HttpUrl("invalid-url"))
 
     def test_optional_statement(self):
-        siwe_message_request = SiweMessageRequest(
-            domain="example.com",
-            address="0x32Be343B94f860124dC4fEe278FDCBD38C102D88",
-            chain_id=1,
-            uri=HttpUrl("https://example.com"),
-        )
+        siwe_message_request = SiweMessageRequestFactory.create(statement=None)
         self.assertIsNone(siwe_message_request.statement)
 
 
@@ -96,14 +69,4 @@ class TestSiweMessageVerificationRequest(unittest.TestCase):
             SiweMessageVerificationRequest(
                 message="Test",
                 signature="a" * 132,
-            )
-
-    def test_invalid_address(self):
-        with self.assertRaises(ValidationError):
-            SiweMessageRequest(
-                domain="example.com",
-                address="0xInvalidEthereumAddress",
-                chain_id=1,
-                uri=HttpUrl("https://example.com"),
-                statement="Test statement",
             )
