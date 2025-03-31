7d414f3b2e44c737721a0db8b0cd96231b44ba08
Moisés <7888669+moisses89@users.noreply.github.com>
2025-03-31 16:16:58 +0200

    Enable SQLmodel and Alembic support (#78)

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter, FastAPI
 
+from starlette.requests import Request
+from starlette.responses import Response
+
 from . import VERSION
+from .datasources.db.connector import db_session, set_database_session_context
 from .routers import about, auth, default
 
 app = FastAPI(
@@ -19,3 +23,26 @@ api_v1_router.include_router(about.router)
 api_v1_router.include_router(auth.router)
 app.include_router(api_v1_router)
 app.include_router(default.router)
+
+
+@app.middleware("http")
+async def http_request_middleware(request: Request, call_next):
+    """
+    Intercepts request and do some actions:
+     - Set the database session context for the current request, so the same database session is used across the whole request.
+
+    :param request:
+    :param call_next:
+    :return:
+    """
+    with set_database_session_context():
+
+        response: Response | None = None
+        try:
+            response = await call_next(request)
+        except Exception as e:
+            raise e
+        finally:
+            await db_session.remove()
+
+            return response
---
+++ app/tests/datasources/db/test_models.py
@@ -0,0 +1,18 @@
+import faker
+
+from app.datasources.db.connector import db_session_context
+from app.datasources.db.models import Users
+
+from .async_db_connector import AsyncDbConnector
+
+fake = faker.Faker()
+
+
+class TestModel(AsyncDbConnector):
+
+    @db_session_context
+    async def test_contract(self):
+        contract = Users(email=fake.email(), hashed_password=fake.password())
+        await contract.create()
+        result = await contract.get_all()
+        self.assertEqual(result[0], contract)
