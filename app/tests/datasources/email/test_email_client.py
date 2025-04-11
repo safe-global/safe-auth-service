b8d2ed9b33dce6bdf5b35bd0ec6b1137904fccaf
Uxio Fuentefria <6909403+Uxio0@users.noreply.github.com>
2025-04-11 14:25:41 +0200

    Add smtp4dev mail service for testing email sending
    - Closes #92

---
+++ app/tests/datasources/email/test_email_client.py
@@ -0,0 +1,59 @@
+import uuid
+from unittest import IsolatedAsyncioTestCase
+
+import aiohttp
+
+from app.config import settings
+from app.datasources.email.email_client import EmailClient
+
+
+class TestEmailClient(IsolatedAsyncioTestCase):
+    """
+    Email sending are tested using smtp4dev
+    """
+
+    async def asyncSetUp(self):
+        deleted_messages = self._delete_messages()
+        self.email_client = EmailClient()
+        await deleted_messages
+
+    async def _delete_messages(self) -> bool:
+        """
+        Delete all messages using smtp4dev API
+
+        Returns:
+            True if all messages were deleted, False otherwise
+        """
+        full_url = settings.SMTP_TEST_API_URL + "Messages/*?mailboxName=Default"
+        async with aiohttp.ClientSession() as session:
+            async with session.delete(full_url) as response:
+                self.assertEqual(200, response.status)
+        return True
+
+    async def _get_number_messages(self) -> int:
+        """
+        Returns:
+            Number of messages using smtp4dev API
+        """
+        full_url = settings.SMTP_TEST_API_URL + "Messages?mailboxName=Default"
+        async with aiohttp.ClientSession() as session:
+            async with session.get(full_url) as response:
+                self.assertEqual(200, response.status)
+                result = await response.json()
+                return result["rowCount"]
+
+    async def test_send_email(self):
+        self.assertEqual(await self._get_number_messages(), 0)
+        sent_successfully = self.email_client.send_email(
+            "random-address@safe.global", "<b>Hello!</b>"
+        )
+        self.assertTrue(sent_successfully)
+        self.assertEqual(await self._get_number_messages(), 1)
+
+    async def test_send_temporary_token_email(self):
+        self.assertEqual(await self._get_number_messages(), 0)
+        sent_successfully = self.email_client.send_temporary_token_email(
+            "random-address@safe.global", uuid.uuid4().hex
+        )
+        self.assertTrue(sent_successfully)
+        self.assertEqual(await self._get_number_messages(), 1)
