880af543ad07df5ce20b2f2caad1cf93eb04b11e
Felipe Alvarado <6717781+falvaradorodriguez@users.noreply.github.com>
2025-04-14 12:03:27 +0200

    Update mail api port and updated env config

--- run_tests.sh
+++ run_tests.sh
@@ -3,9 +3,9 @@
 set -euo pipefail
 
 export ENV_FILE=.env.test
-docker compose -f docker-compose.yml build --force-rm redis db apisix
-docker compose -f docker-compose.yml up --no-start redis db apisix
-docker compose -f docker-compose.yml start redis db apisix
+docker compose -f docker-compose.yml build --force-rm redis db apisix mail
+docker compose -f docker-compose.yml up --no-start redis db apisix mail
+docker compose -f docker-compose.yml start redis db apisix mail
 
 sleep 10
 # Run migrations
